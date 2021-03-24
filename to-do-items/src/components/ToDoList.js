d268e40a820a9e3bfe98423c369fa518fa30a826
GuidoLandoni <guidolandoni97@gmail.com>
2021-03-24 17:59:20 -0300

    add fetch

--- to-do-items/src/components/ToDoList.js
+++ to-do-items/src/components/ToDoList.js
@@ -1,35 +1,83 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ToDoForm from './ToDoForm';
 import ToDo from './ToDo';
 
 function ToDoList() {
     const [toDos, setToDos] = useState([]);
 
-    const addToDo = toDo =>{
+    useEffect(()=>{
+        const getToDos = async() =>{
+            const toDosFromServer = await fetchToDos();
+            setToDos(toDosFromServer);
+        }
+        getToDos();
+    }, [])
+
+    const fetchToDos = async(id) =>{
+        const res = await fetch(`http://localhost:5000/api/item/get`, {
+            method: 'GET',
+            headers:{
+                'Content-type':'application/json',
+            },
+            body: JSON.stringify(id),
+        })
+        const data = await res.json();
+        return data
+    };
+
+    const addToDo = async(toDo) =>{
         if(!toDo.text){
             return;
         }
-        const newToDos = [toDo, ...toDos];
+        const res = await fetch(`http://localhost:5000/api/item/add`, {
+            method: 'POST',
+            headers: {
+                'Content-type':'application/json',
+                },
+            body: JSON.stringify(toDo),
+        })
 
-        setToDos(newToDos);
+        const data = await res.json()
+        setToDos([...toDos, data])
     };
 
-    const removeToDo = id =>{
+    const removeToDo = async(id) =>{
+        await fetch(`http://localhost:5000/api/item/delete`, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(id),
+        })
         const removeById = [...toDos].filter(toDo => toDo.id !== id)
         setToDos(removeById);
     };
 
-    const editToDo = (toDoId, newValue) =>{
+    const editToDo = async (toDoId, newValue) =>{
         if(!newValue.text){
             return;
-        }
-        setToDos(prev => prev.map(item => (item.id === toDoId ? newValue : item))); 
+        }       
+        setToDos(prev => prev.map(item => (item.id === toDoId ? newValue : item)));
+        await fetch(`http://localhost:5000/api/item/edit`) 
     };
 
-    const completeToDos = id =>{
+    const completeToDos = async(id) =>{
+        const toDoComplete = await fetchToDos(id)
+        const editedToDo = {...toDoComplete, done: !toDoComplete.done}
+
+        const res = await fetch(`https://localhost:5000/api/item/edit`,{
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(editedToDo),
+        })
+
+        const data = await res.json()
+
         let updatedToDos = toDos.map(toDo => {
             if (toDo.id === id){
-                toDo.done = !toDo.done
+                toDo.done = data.done
                 toDo.isComplete = !toDo.isComplete;
             }
             return toDo;
